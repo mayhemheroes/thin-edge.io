d0df45663d9b52d387260dbb4268b0eebbc684df
Aaron Steele <ajsteele@asu.edu>
2022-06-28 20:07:23 -0700

    Fixed fuzzing binaryy to work properly

--- crates/core/c8y_translator/fuzz/fuzz_targets/fuzz_target_1.rs
+++ crates/core/c8y_translator/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -4,5 +4,7 @@ use libfuzzer_sys::fuzz_target;
 use c8y_translator::json;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = json::from_thin_edge_json(std::str::from_utf8(data).unwrap());
+    if let Ok(s) = std::str::from_utf8(data) {
+        let _ = json::from_thin_edge_json(s);
+    }
 });
